9fd3c7bb3faec6ccd6b2c9cc5548c84394476272
drebelsky <drebelsky@gmail.com>
2015-01-17 07:48:19 -0600

    Added functionality for file submitting

---
+++ base.js
@@ -0,0 +1,29 @@
+document.body.onload = initialize;
+function initialize() {
+  var files = document.getElementById("files");
+  files.onchange = function() {
+    var length = this.files.length;
+    var fileReaders  = [];
+    for(var fileIndex = 0; fileIndex < length; fileIndex++) {
+      fileReaders.push(new FileReader());
+      fileReaders[fileIndex].onloadend = function() {
+        var image = document.createElement("img");
+        image.src = this.result;
+        document.getElementById("previewImages").appendChild(image);
+      }
+      if(this.files[fileIndex]) {
+        fileReaders[fileIndex].readAsDataURL(this.files[fileIndex]);
+      }
+    }
+  }
+  add = document.getElementById("add");
+  add.onclick = function() {
+    preview = document.getElementById("previewImages");
+    var children = preview.children;
+    var times = children.length;
+    for(var i = 0; i < times; i++) {
+      console.log(children.length);
+      document.body.appendChild(preview.removeChild(children[0]));
+    }
+  }
+}
