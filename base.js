0ec71c2d42c47cfb5f46002ec029f78c81972658
drebelsky <drebelsky@gmail.com>
2015-01-18 19:45:19 -0600

    Added rotation support

--- base.js
+++ base.js
@@ -1,4 +1,7 @@
 var objects = [];
+var htmlObjects = [];
+var active = -1;
+var key = "";
 document.body.onload = initialize;
 function initialize() {
   var files = document.getElementById("files");
@@ -31,8 +34,14 @@ function initialize() {
     var newObject;
     for(var i = 0; i < times; i++) {
       newObject = preview.removeChild(children[0]).children[0];
-      newObject.setAttribute("data-spot", objects.length);
-      objects.push([]);
+      newObject.setAttribute("data-position", objects.length);
+      objects.push(new Array(parseInt(document.getElementById("frames").value)));
+      htmlObjects.push(newObject);
+      document.body.appendChild(newObject);
+      newObject.onclick = function() {
+        active = parseInt(this.getAttribute("data-position"));
+        drawFrameSeperators();
+      }
       document.body.appendChild(newObject);
     }
   }
@@ -51,8 +60,19 @@ function initialize() {
   function drawFrameSeperators() {
     var frames = parseInt(document.getElementById("frames").value);
     timelineContext.fillStyle = "lightgrey";
-    for(var x=0; x < timeline.width; x += (timeline.width/frames)) {
-      timelineContext.fillRect(x, 0, 1, timeline.height);
+    if(active != -1) {
+      for(var x=0; x < timeline.width; x += (timeline.width/frames)) {
+        if(objects[active][x] != undefined) {
+          timelineContext.fillStyle = "yellow";
+        }
+        timelineContext.fillRect(x, 0, 1, timeline.height);
+        timelineContext.fillStyle = "lightgrey";
+      }
+    }
+    else {
+      for(var x=0; x < timeline.width; x += (timeline.width/frames)) {
+        timelineContext.fillRect(x, 0, 1, timeline.height);
+      }
     }
   }
   drawFrameSeperators();
@@ -64,11 +84,63 @@ function initialize() {
     timeline.height = window.innerHeight / 20;
     timelineContext.clearRect(0, 0, timeline.width, timeline.height);
     drawFrameSeperators();
-    timelineContext.fillStyle = "yellow";
+    timelineContext.fillStyle = "green";
     timelineContext.fillRect(Math.round(x / timeline.width * frames) * timeline.width/frames, 0, 1, timeline.height);
   }
   timeline.onmousemove = updateTimeline;
   document.getElementById("frames").onchange = function() {
+    for(var objectIndex = 0; objectIndex < objects.length; objectIndex++) {
+      if(objects[objectIndex].length < parseInt(this.value)) {
+        objects[objectIndex] = objects[objectIndex].concat(new Array(parseInt(this.value) - objects[objectIndex].length));
+      }
+    }
     updateTimeline();
+  }
+}
+document.body.onkeydown = function(event) {
+  var keycode = event.which || event.keyCode || event.charCode;
+  if(keycode == 82) {
+    key = "r";
+  }
+  if(keycode == 71) {
+    key = "g";
+  }
+  if(keycode == 83) {
+    key = "s";
+  }
+}
+document.body.onkeyup = function(event) {
+  var keycode = event.which || event.keyCode || event.charCode;
+  if(keycode == 82 && key == "r") {
+    key = "";
+  }
+  if(keycode == 71 && key == "g") {
+    key = "";
+  }
+  if(keycode == 83 && key == "s") {
+    key = "";
+  }
+}
+document.body.onmousemove = function(event) {
+  if(active != -1 && key != "") {
+    var activeNode = htmlObjects[active]
+    var center = {x: activeNode.style.left + activeNode.width / 2, y: activeNode.style.top + activeNode.height / 2};
+    if(key == "r") {
+      var rotation;
+      if(event.pageX < center.x) {
+        rotation = Math.atan((event.pageY - center.y) / (event.pageX-center.x)) - Math.PI / 2;
+      }
+      else {
+        rotation = Math.atan((event.pageY - center.y) / (event.pageX-center.x)) + Math.PI / 2;
+      }
+      var degrees = rotation * 180 / Math.PI;
+      activeNode.style.webkitTransform = "rotate("+degrees+"deg)";
+    }
+    if(key == "g") {
+
+    }
+    if(key == "s") {
+
+    }
   }
 }
